d9d0ff57e47d059981cffd822dfa3c5fdc3433dc
kirellosGhobrial <57769412+kirellosGhobrial@users.noreply.github.com>
2022-12-07 08:30:16 +0100

    deleting unnecessary comments

--- src/main/java/com/pld/agile/view/map/MarkersPainter.java
+++ src/main/java/com/pld/agile/view/map/MarkersPainter.java
@@ -11,57 +11,16 @@ package com.pld.agile.view.map;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+
 
 import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.viewer.WaypointPainter;
-import org.jxmapviewer.viewer.WaypointRenderer;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
 
 /**
  * A fancy waypoint painter
- * @author Martin Steiger
  */
-public class MarkersPainter extends WaypointPainter<Marker>
-{
-
-//    private final Map<Color, BufferedImage> map = new HashMap<Color, BufferedImage>();
-
-//    private final Font font = new Font("Lucida Sans", Font.BOLD, 10);
-
-//    private BufferedImage origImage;
-
-    /**
-     * Uses a default waypoint image
-     */
-
-//    private static BufferedImage convert(BufferedImage loadImg, Color newColor)
-//    {
-//        int w = loadImg.getWidth();
-//        int h = loadImg.getHeight();
-//        BufferedImage imgOut = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-//        BufferedImage imgColor = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-//
-//        Graphics2D g = imgColor.createGraphics();
-//        g.setColor(newColor);
-//        g.fillRect(0, 0, w+1, h+1);
-//        g.dispose();
-//
-//        Graphics2D graphics = imgOut.createGraphics();
-//        graphics.drawImage(loadImg, 0, 0, null);
-//        graphics.setComposite(MultiplyComposite.Default);
-//        graphics.drawImage(imgColor, 0, 0, null);
-//        graphics.dispose();
-//
-//        return imgOut;
-//    }
-
+public class MarkersPainter extends WaypointPainter<Marker> {
 
     @Override
     protected void doPaint(Graphics2D g, JXMapViewer viewer, int width, int height) {
@@ -84,13 +43,6 @@ public class MarkersPainter extends WaypointPainter<Marker>
             g.setColor(c);
         }
         for (Marker w : getWaypoints()) {
-//            BufferedImage myImg = map.get(w.getColor());
-//
-//            if (myImg == null)
-//            {
-//                myImg = convert(origImage, w.getColor());
-//                map.put(w.getColor(), myImg);
-//            }
             Point2D point = viewer.getTileFactory().geoToPixel(w.getPosition(), viewer.getZoom());
             int zoom = viewer.getZoom();
             int imgW = 100/(zoom+1);
@@ -100,11 +52,6 @@ public class MarkersPainter extends WaypointPainter<Marker>
             int y = (int)(point.getY() - rectangle.getY());
             Image img = w.getImg().getScaledInstance(imgW,imgH,Image.SCALE_SMOOTH);
             g.drawImage(img, x -img.getWidth(null) / 2, y -img.getHeight(null), null);
-//            JLabel lbl = w.getLbl();
-//            lbl.setBounds( x-imgW/2, y-imgH/2, imgW,imgH);
-//            lbl.setBackground(w.getColor());
-//            lbl.setOpaque(true);
-//            viewer.add(lbl);
         }
     }
 
