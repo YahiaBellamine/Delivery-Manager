e38975999e344bc379bcc7c4985c4a846124db3f
HubNBr <79989810+HubNBr@users.noreply.github.com>
2022-12-13 21:11:21 +0100

    feat: Added code for delete and update in the button listener. status: not tested

--- src/main/java/com/pld/agile/view/listener/ButtonListener.java
+++ src/main/java/com/pld/agile/view/listener/ButtonListener.java
@@ -1,11 +1,15 @@
 package com.pld.agile.view.listener;
 
 import com.pld.agile.controller.Controller;
+import com.pld.agile.model.Courier;
+import com.pld.agile.model.Couriers;
+import com.pld.agile.model.enums.TimeWindow;
 import com.pld.agile.view.DeliveriesView;
 import com.pld.agile.view.Window;
 import com.pld.agile.view.map.MapViewer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.*;
@@ -25,6 +29,8 @@ public class ButtonListener implements ActionListener {
   public void actionPerformed(ActionEvent e) {
     // Method called by the button listener each time a button is clicked
     // Forward the corresponding message to the controller
+    JComponent source = (JComponent) e.getSource();
+
     switch (e.getActionCommand()) {
       case Window.LOAD_MAP:
         controller.loadMap();
@@ -51,55 +57,104 @@ public class ButtonListener implements ActionListener {
         break;
 
       case DeliveriesView.UPDATE_DELIVERY_REQUEST:
-        /*controller.updateDeliveryRequest()*/
-        System.out.println("Update a delivery request");
+        JComboBox<TimeWindow> comboBoxTimeWindow = new JComboBox<>();
+        ((JLabel) comboBoxTimeWindow.getRenderer()).setHorizontalAlignment(JLabel.LEFT);
+        for(TimeWindow timeWindow : TimeWindow.values()) {
+          comboBoxTimeWindow.addItem(timeWindow);
+        }
+
+        JComboBox<Courier> comboBoxCourier = new JComboBox<>();
+        ((JLabel) comboBoxCourier.getRenderer()).setHorizontalAlignment(JLabel.LEFT);
+        for(Courier courier : Couriers.courierList) {
+          comboBoxCourier.addItem(courier);
+        }
+
+        final JComponent[] inputs = new JComponent[] {
+                new JLabel("New Courier:"),
+                comboBoxCourier,
+                new JLabel("New Time Window:"),
+                comboBoxTimeWindow
+        };
+        int result = JOptionPane.showConfirmDialog(null, inputs, "Update delivery request", JOptionPane.PLAIN_MESSAGE);
+        if (result == JOptionPane.OK_OPTION) {
+          //Get delivery and previous courier
+          int deliveryId = getOriginDelivery(source);
+          int previousCourierId = getOriginCourier(source);
+          Courier previousCourier = Couriers.courierList.get(previousCourierId);
+
+          //Get the new courier and time window
+          Courier selectedCourier = (Courier) comboBoxCourier.getSelectedItem();
+          TimeWindow selectedTimeWindow = (TimeWindow) comboBoxTimeWindow.getSelectedItem();
+          //Change method signature so that it takes the previous courier, the new courier, the new time window and the delivery id(position in the tour list)
+          /*controller.updateDeliveryRequest();*/
+        }
         break;
 
       case DeliveriesView.DELETE_DELIVERY_REQUEST:
-        int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this delivery request ?", "Confirm delete", JOptionPane.YES_NO_OPTION);
+        int userChoice = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this delivery request ?", "Confirm delete", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
         //Validate delete
-        if(result == 0) {
-          System.out.println("Delete a delivery request");
+        if(userChoice == JOptionPane.YES_OPTION) {
+          int deliveryNumber = getOriginDelivery(source);
+          int courierId = getOriginCourier(source);
+          Courier deliveryCourier = Couriers.courierList.get(courierId);
 
-          JComponent source = (JComponent) e.getSource();
-          Container sourceParent = source.getParent();
-          System.out.println(sourceParent.getName());
-          Component[] components = sourceParent.getComponents();
-          int deliveryNumber = -1;
-          int courierId = -1;
-          for(Component component : components) {
-            if(component.getName() == "detailsTitle") {
-              Pattern stringToNumber = Pattern.compile("Delivery n°\\d+");
-              Matcher nameMatcher = stringToNumber.matcher(((JLabel) component).getText());
-
-              if(nameMatcher.find()) {
-                Pattern numberToInt = Pattern.compile("\\d+");
-                Matcher deliveryMatcher = numberToInt.matcher(nameMatcher.group(0));
-                System.out.println(nameMatcher.group(0));
-
-                if (deliveryMatcher.find()) {
-                  deliveryNumber = Integer.parseInt(deliveryMatcher.group(0));
-                }
-              }
-            }
-            if(component.getName() == "detailsCourier") {
-              Pattern stringToNumber = Pattern.compile("Courier (id): \\d+");
-              Matcher nameMatcher = stringToNumber.matcher(((JLabel) component).getText());
-
-              if(nameMatcher.find()) {
-                Pattern numberToInt = Pattern.compile("\\d+");
-                Matcher deliveryMatcher = numberToInt.matcher(nameMatcher.group(0));
-                System.out.println(nameMatcher.group(0));
-
-                if (deliveryMatcher.find()) {
-                  courierId = Integer.parseInt(deliveryMatcher.group(0));
-                }
-              }
-            }
-          }
-          //controller.deleteDeliveryRequest(courierId, deliveryNumber);
+          controller.deleteDeliveryRequest(deliveryCourier, deliveryNumber);
         }
-      break;
+        break;
     }
+  }
+
+  /**
+   * Returns the delivery linked to an event source.
+   * @param source The source we want to retrieve the delivery from.
+   * @return The id of the delivery.
+   */
+  private int getOriginDelivery(JComponent source) {
+    Container sourceParent = source.getParent();
+    Component[] components = sourceParent.getComponents();
+    int deliveryNumber = -1;
+    for(Component component : components) {
+      if (Objects.equals(component.getName(), "detailsTitle")) {
+        Pattern stringToNumber = Pattern.compile("Delivery n°\\d+");
+        Matcher nameMatcher = stringToNumber.matcher(((JLabel) component).getText());
+
+        if (nameMatcher.find()) {
+          Pattern numberToInt = Pattern.compile("\\d+");
+          Matcher deliveryMatcher = numberToInt.matcher(nameMatcher.group(0));
+
+          if (deliveryMatcher.find()) {
+            deliveryNumber = Integer.parseInt(deliveryMatcher.group(0));
+          }
+        }
+      }
+    }
+    return deliveryNumber;
+  }
+
+  /**
+   * Returns the courier linked to an event source.
+   * @param source The source we want to retrieve the courier from.
+   * @return The id of the courier.
+   */
+  private int getOriginCourier(JComponent source) {
+    Container sourceParent = source.getParent();
+    Component[] components = sourceParent.getComponents();
+    int courierId = -1;
+    for(Component component : components) {
+      if(Objects.equals(component.getName(), "detailsCourier")) {
+        Pattern stringToNumber = Pattern.compile("Courier \\(id\\): \\d+");
+        Matcher nameMatcher = stringToNumber.matcher(((JLabel) component).getText());
+
+        if(nameMatcher.find()) {
+          Pattern numberToInt = Pattern.compile("\\d+");
+          Matcher deliveryMatcher = numberToInt.matcher(nameMatcher.group(0));
+
+          if (deliveryMatcher.find()) {
+            courierId = Integer.parseInt(deliveryMatcher.group(0));
+          }
+        }
+      }
+    }
+    return courierId;
   }
 }
