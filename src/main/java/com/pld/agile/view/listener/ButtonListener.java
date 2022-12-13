c7d23bdac59bebdc816810dea0e3f299c80513c1
HubNBr <79989810+HubNBr@users.noreply.github.com>
2022-12-13 16:11:30 +0100

    feat: Added call to delete in button listener. status: not tested. important: commented call @ButtonListener - actionPerformed - case DeleteDeliveryRequest

--- src/main/java/com/pld/agile/view/listener/ButtonListener.java
+++ src/main/java/com/pld/agile/view/listener/ButtonListener.java
@@ -4,9 +4,12 @@ import com.pld.agile.controller.Controller;
 import com.pld.agile.view.DeliveriesView;
 import com.pld.agile.view.Window;
 import com.pld.agile.view.map.MapViewer;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.*;
+import java.awt.*;
 
 public class ButtonListener implements ActionListener {
 
@@ -23,14 +26,80 @@ public class ButtonListener implements ActionListener {
     // Method called by the button listener each time a button is clicked
     // Forward the corresponding message to the controller
     switch (e.getActionCommand()) {
-      case Window.LOAD_MAP -> controller.loadMap();
-      case Window.ADD_DELIVERY_REQUEST -> controller.addNewRequest();
-      case Window.SAVE_TOURS -> controller.saveTours()/*System.out.println("Save tours call")*/;
-      case Window.LOAD_TOURS -> controller.loadTours()/*System.out.println("Load tours call")*/;
-      case Window.RECENTER_MAP -> mapViewer.recenter();
-      case Window.ADD_COURIER ->/* controller.addCourier()*/System.out.println("Add a courier");
-      case DeliveriesView.UPDATE_DELIVERY_REQUEST -> /*controller.updateDeliveryRequest()*/System.out.println("Update a delivery request");
-      case DeliveriesView.DELETE_DELIVERY_REQUEST -> /*controller.deleteDeliveryRequest()*/System.out.println("Delete a delivery request");
+      case Window.LOAD_MAP:
+        controller.loadMap();
+        break;
+
+      case Window.ADD_DELIVERY_REQUEST:
+        controller.addNewRequest();
+        break;
+
+      case Window.SAVE_TOURS:
+        controller.saveTours()/*System.out.println("Save tours call")*/;
+        break;
+
+      case Window.LOAD_TOURS:
+        controller.loadTours()/*System.out.println("Load tours call")*/;
+        break;
+
+      case Window.RECENTER_MAP:
+        mapViewer.recenter();
+        break;
+
+      case Window.ADD_COURIER:
+        /* controller.addCourier()*/System.out.println("Add a courier");
+        break;
+
+      case DeliveriesView.UPDATE_DELIVERY_REQUEST:
+        /*controller.updateDeliveryRequest()*/
+        System.out.println("Update a delivery request");
+        break;
+
+      case DeliveriesView.DELETE_DELIVERY_REQUEST:
+        int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this delivery request ?", "Confirm delete", JOptionPane.YES_NO_OPTION);
+        //Validate delete
+        if(result == 0) {
+          System.out.println("Delete a delivery request");
+
+          JComponent source = (JComponent) e.getSource();
+          Container sourceParent = source.getParent();
+          System.out.println(sourceParent.getName());
+          Component[] components = sourceParent.getComponents();
+          int deliveryNumber = -1;
+          int courierId = -1;
+          for(Component component : components) {
+            if(component.getName() == "detailsTitle") {
+              Pattern stringToNumber = Pattern.compile("Delivery n°\\d+");
+              Matcher nameMatcher = stringToNumber.matcher(((JLabel) component).getText());
+
+              if(nameMatcher.find()) {
+                Pattern numberToInt = Pattern.compile("\\d+");
+                Matcher deliveryMatcher = numberToInt.matcher(nameMatcher.group(0));
+                System.out.println(nameMatcher.group(0));
+
+                if (deliveryMatcher.find()) {
+                  deliveryNumber = Integer.parseInt(deliveryMatcher.group(0));
+                }
+              }
+            }
+            if(component.getName() == "detailsCourier") {
+              Pattern stringToNumber = Pattern.compile("Courier (id): \\d+");
+              Matcher nameMatcher = stringToNumber.matcher(((JLabel) component).getText());
+
+              if(nameMatcher.find()) {
+                Pattern numberToInt = Pattern.compile("\\d+");
+                Matcher deliveryMatcher = numberToInt.matcher(nameMatcher.group(0));
+                System.out.println(nameMatcher.group(0));
+
+                if (deliveryMatcher.find()) {
+                  courierId = Integer.parseInt(deliveryMatcher.group(0));
+                }
+              }
+            }
+          }
+          //controller.deleteDeliveryRequest(courierId, deliveryNumber);
+        }
+      break;
     }
   }
 }
